604e777763395e1d6f13cce601ceb1f87d7ba04d
Wanny Martins <wcatery@gmail.com>
2022-01-17 14:12:58 -0300

    finailzando exercicio 2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('parameters must be numbers');
+        }
+      
+        return a + b;
+    
+}
+
+        
+
+
+ 
+      // implemente seus testes aqui
+
+      module.exports = sum;
