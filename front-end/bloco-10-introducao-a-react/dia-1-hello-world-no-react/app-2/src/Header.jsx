a1130f251b5095352e62b83e76826cf68bd8ac43
Wanny Martins <wcatery@gmail.com>
2022-02-17 12:48:13 -0300

    feito todos os exercicios do dia1

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app-2/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 className='title'>Conteúdos de Front-End</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Header;
