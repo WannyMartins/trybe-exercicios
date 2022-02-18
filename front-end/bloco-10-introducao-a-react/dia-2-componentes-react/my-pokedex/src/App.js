d1de96d96506873ff4338714e69929c4ce5aa190
Wanny Martins <wcatery@gmail.com>
2022-02-18 12:53:04 -0300

    pokedex pronta

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pokedex from './Components/Pokedex';
+
+function App() {
+  return (
+    <div >
+      <h1>Pokédex</h1>      
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
