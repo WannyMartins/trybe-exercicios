9186a908e8c448dca31a48a0272219ef1a797501
Wanny Martins <wcatery@gmail.com>
2022-02-22 12:28:02 -0300

    pokedex com filtro

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokedex from './Pokedex';
+
+class App extends React.Component {
+  render(){
+  return (
+    <Pokedex />
+  );
+}}
+
+
+export default App;
